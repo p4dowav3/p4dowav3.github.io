125e64c7ad1dfd4a31df1955fabf61bd20a97a10
p4dowav3 <p4dowav3@naver.com>
2024-10-18 10:51:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
-document.addEventListener('click', (e) => {
-    if (e.target.tagName === 'A') {
-        e.preventDefault();
-        const href = e.target.getAttribute('href');
-        loadPage(href);
-    }
-});
-
-function loadPage(url) {
-    fetch(url)
-        .then(response => response.text())
-        .then(html => {
-            const parser = new DOMParser();
-            const doc = parser.parseFromString(html, 'text/html');
-            const newContent = doc.querySelector('.content-wrapper').innerHTML;
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelectorAll('a').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
             
-            document.body.style.opacity = 0;
+            document.body.classList.add('fade-out');
             
-            setTimeout(() => {
-                document.querySelector('.content-wrapper').innerHTML = newContent;
-                
-                history.pushState(null, '', url);
-                
-                document.title = doc.title;
-                
-                document.body.style.opacity = 1;
-            }, 300);
+            let href = this.getAttribute('href');
+            
+            setTimeout(function () {
+                loadPage(href);
+            }, 1500);
         });
-}
+    });
 
-window.addEventListener('popstate', () => {
-    loadPage(window.location.pathname);
+    function loadPage(url) {
+        fetch(url)
+            .then(response => response.text())
+            .then(html => {
+                const parser = new DOMParser();
+                const doc = parser.parseFromString(html, 'text/html');
+                const newContent = doc.querySelector('.content-wrapper').innerHTML;
+                
+                document.body.style.opacity = 0;
+                
+                setTimeout(() => {
+                    document.querySelector('.content-wrapper').innerHTML = newContent;
+                    
+                    history.pushState(null, '', url);
+                    
+                    document.title = doc.title;
+                    
+                    document.body.style.opacity = 1;
+                    
+                    document.body.classList.remove('fade-out');
+                }, 300);
+            });
+    }
+
+    window.addEventListener('popstate', () => {
+        loadPage(window.location.pathname);
+    });
 });
