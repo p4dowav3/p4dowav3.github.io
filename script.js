33275d6c3da04f97bda780b8ff8690e65ed3f5c7
p4dowav3 <p4dowav3@naver.com>
2024-10-18 11:20:28 +0900

    Update script.js

--- script.js
+++ script.js
@@ -13,41 +13,42 @@ document.addEventListener('DOMContentLoaded', (event) => {
         });
     });
 
-function loadPage(url) {
-    fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.text();
-        })
-        .then(html => {
-            const parser = new DOMParser();
-            const doc = parser.parseFromString(html, 'text/html');
+    function loadPage(url) {
+        fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.text();
+            })
+            .then(html => {
+                const parser = new DOMParser();
+                const doc = parser.parseFromString(html, 'text/html');
+                
+                const newContent = doc.querySelector('.content-wrapper');
+                if (newContent) {
+                    document.querySelector('.content-wrapper').innerHTML = newContent.innerHTML;
+                }
 
-            const newContent = doc.querySelector('.content-wrapper');
-            if (newContent) {
-                document.querySelector('.content-wrapper').innerHTML = newContent.innerHTML;
-            }
+                const newLangMenu = doc.querySelector('.lang-menu');
+                if (newLangMenu) {
+                    document.querySelector('.lang-menu').innerHTML = newLangMenu.innerHTML;
+                }
 
-            const newLangMenu = doc.querySelector('.lang-menu');
-            if (newLangMenu) {
-                document.querySelector('.lang-menu').innerHTML = newLangMenu.innerHTML;
-            }
+                history.pushState(null, '', url);
 
-            history.pushState(null, '', url);
+                document.title = doc.title;
 
-            document.title = doc.title;
-
-            document.body.style.opacity = 1;
-            document.body.classList.remove('fade-out');
-        })
-        .catch(error => {
-            console.error('Fetch error:', error);
-            window.location.href = url;
-        });
-}
-
+                setTimeout(() => {
+                    document.body.style.opacity = 1;
+                    document.body.classList.remove('fade-out');
+                }, 300);
+            })
+            .catch(error => {
+                console.error('Fetch error:', error);
+                window.location.href = url;
+            });
+    }
 
     window.addEventListener('popstate', () => {
         loadPage(window.location.pathname);
